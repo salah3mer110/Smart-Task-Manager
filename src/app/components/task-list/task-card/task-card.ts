66011e40980f2027490db5e654594b6772254847
salah3mer110 <salahamer3040@gmail.com>
2026-06-11 14:01:56 +0300

    Adding Task-list page

---
+++ src/app/components/task-list/task-card/task-card.ts
@@ -0,0 +1,49 @@
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { DatePipe } from '@angular/common';
+
+import { Task } from '../../task.model';
+import { TasksService } from '../../tasksService.service';
+
+@Component({
+  selector: 'app-task-card',
+  imports: [DatePipe],
+  templateUrl: './task-card.html',
+  styleUrl: './task-card.css',
+})
+export class TaskCard implements OnDestroy, OnChanges {
+  @Input({ required: true }) task!: Task;
+  @Output() edit = new EventEmitter<Task>();
+  private tasksService = inject(TasksService);
+  onDeleteTask() {
+    this.tasksService.deleteTask(this.task.id);
+  }
+  onCompleteTask() {
+    this.tasksService.markTaskComplete(this.task.id);
+  }
+
+  onEditTask() {
+    this.edit.emit(this.task);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const change = changes['task'];
+    if (change && !change.firstChange) {
+      if (change.currentValue.status !== change.previousValue.status) {
+        console.log(`${this.task.title} task has been completed!`);
+      } else console.log(`${this.task.title} task has been updated!`);
+    }
+  }
+
+  ngOnDestroy(): void {
+    console.log(`${this.task.title} task has been destroyed`);
+  }
+}
---
+++ src/app/components/task-list/task-list.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { TaskCard } from './task-card/task-card';
+import { Task } from '../task.model';
+import { TasksService } from '../tasksService.service';
+
+@Component({
+  selector: 'app-task-list',
+  imports: [TaskCard],
+  templateUrl: './task-list.html',
+  styleUrl: './task-list.css',
+})
+export class TaskList {
+  @Output() edit = new EventEmitter<Task>();
+  tasksStatus = 'all';
+  taskservices = inject(TasksService);
+
+  get filteredTasks() {
+    if (this.tasksStatus === 'done')
+      return this.taskservices.tasks.filter((task) => task.status === 'done');
+    if (this.tasksStatus === 'notDone')
+      return this.taskservices.tasks.filter((task) => task.status === 'notDone');
+    return this.taskservices.tasks;
+  }
+}
